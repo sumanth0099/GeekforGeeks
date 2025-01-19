103dc054c9566a8f5e4167f41fe775f9b8cbb93e
Tetala Sumanth Reddy <142477844+sumanth0099@users.noreply.github.com>
2025-01-19 16:41:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Pattern 7/pattern-7.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution obj = new Solution();
+            obj.printTriangle(n);
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+   public static void Gaps(int size)
+     {
+        for(int i=0;i<size;i++)
+        System.out.print(" ");
+     }
+    void printTriangle(int n) {
+        String arr = "*";
+        for(int i=0;i<n;i++)
+        {
+            Solution.Gaps(n-1-i);
+            System.out.println(arr);
+            arr+="**";
+        }
+    }
+}
+
+
+
+
