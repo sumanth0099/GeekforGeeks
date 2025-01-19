f5c2da81def81a4b382bb052ab6ae9c6bad5f569
Tetala Sumanth Reddy <142477844+sumanth0099@users.noreply.github.com>
2025-01-19 16:50:06 +0530

    Update pattern-7.java

--- Difficulty: Basic/Pattern 7/pattern-7.java
+++ Difficulty: Basic/Pattern 7/pattern-7.java
@@ -28,13 +28,11 @@ class Solution {
      }
     void printTriangle(int n) {
         String arr = "*";
-        String bh=arr;
         for(int i=0;i<n;i++)
         {
             Solution.Gaps(n-1-i);
             System.out.println(arr);
             arr+="**";
-            bh="*";
         }
     }
 }
